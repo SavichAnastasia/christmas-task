6b1516cb6b755388ec950109838d4525050c8a0d
asavich <anastasiya.savich@burberry.com>
2021-12-29 19:34:04 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+    const isProduction = options.mode === 'production';
+  
+    const config = {
+        mode: isProduction ? 'production' : 'development',
+        devtool: isProduction ? 'none' : 'inline-source-map',
+        entry: './src/index.js',
+        output: {
+            path: path.resolve('__dirname', 'public'),
+            filename: '[name].[hash].js',
+            clean: true,
+        },
+        devServer: {
+            static: {
+              directory: path.join('__dirname', 'public'),
+            },
+            compress: true,
+            port: 3000,
+        },
+        module: {
+            rules: [
+              {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }],
+                      "@babel/preset-react",
+                      '@babel/preset-typescript'
+                    ]
+                  }
+                }
+              },
+              {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  "style-loader",
+                  "css-loader",
+                  "sass-loader"
+                ]
+              },
+              {
+                test: /\.tsx?$/,
+                use: [
+                  {
+                    loader: 'ts-loader'
+                  }
+                ]
+              },
+            ]
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+          }),
+        ]
+    }
+
+    return config;
+}
